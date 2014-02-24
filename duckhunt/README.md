3d9ea8a8a72ca64795e49cea1502f7149fcd6ecd
zrmcgow01 <zachary.mcgowan@tufts.edu>
2014-02-24 13:16:29 -0500

    birds, tree, and dog

---
+++ duckhunt/README.md
@@ -0,0 +1,5 @@
+//assignment 2: Duck Hunt, part 1
+//By: Zach McGowan
+//Correctly implemented parts: *__*
+//All work done for this project was done working alone
+//Approximate number of hours spent on project: *__*
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,16 @@
-// Your work goes here...
+
+function draw(){
+	canvas = document.getElementById('game');
+	ctx = canvas.getContext('2d');
+	var img = new Image();
+	img.onload = function(){
+		for(var i = 1; i < 6; i++){
+			ctx.drawImage(img, 120, 110, 50, 40, 100*i, 100, 50, 40);
+		}
+		ctx.drawImage(img, 0, 0, 60, 45, 350, 455, 60, 45);
+		ctx.drawImage(img, 0, 270, 70, 130, 100 , 240, 140, 260);
+		ctx.fillStyle = "rgb(200,0,0)";
+		ctx.fillRect(200, 200, 1, 1);
+	}
+	img.src = 'assets/duckhunt.png/';
+}
