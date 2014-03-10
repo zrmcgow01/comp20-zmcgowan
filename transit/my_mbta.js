72d32f29ab8b301bd2a720c9746f040342a0b8f2
zrmcgow01 <zachary.mcgowan@tufts.edu>
2014-03-10 12:36:30 -0400

    init and displayMap code

--- transit/my_mbta.js
+++ transit/my_mbta.js
@@ -1,1 +1,47 @@
 //My Javascript function code here....
+var map; //global variable for Google map
+var lat = 0; //initialize latitiude variable
+var lon = 0; //initialize longitude variable
+var infoWindow = new google.maps.InfoWindow(); //this will be filled
+						//with information about the appropriate station
+var myLocation = new google.maps.LatLng(lat, lon);
+var mapOptions = {
+					zoom: 13,
+					center: myLocation,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+				
+
+function init(){
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position){
+		lat = position.coords.latitude;
+		lon = position.coords.longitude;
+		displayMap();
+		});
+	}
+	else {
+		alert("Geolocation is not supported by this browser.");
+		}
+}
+
+
+function displayMap(){
+	myLocation = new google.maps.LatLng(lat, lon);
+	
+	map.panTo(myLocation);
+	console.log("I'm here");
+	marker = new google.maps.Marker({
+		position: myLocation,
+		title: "I am here!"
+	});
+	marker.setMap(map);
+	
+	//google.maps.event.addListener(marker, 'click', function() {
+	//	infoWindow.setContent(marker.title);
+	//	infoWindow.open(map, marker);
+	//});
+	
+	
+}
