d4394c7edb59d7e719704047ccc9edc4ff5325db
zrmcgow01 <zachary.mcgowan@tufts.edu>
2014-03-11 15:46:15 -0400

    added json file and debug parse json

--- transit/my_mbta.js
+++ transit/my_mbta.js
@@ -1,7 +1,5 @@
-var request = new XMLHttpRequest();
 
-
-
+var xhr;
 
 function init(){
 	var lat = 42.3599611; //this will be changed to 0 in final version
@@ -12,13 +10,13 @@ function init(){
 						center: myLocation,
 						mapTypeId: google.maps.MapTypeId.ROADMAP
 					};
-	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);			
+	map_div = document.getElementById("map_canvas");
+	var map = new google.maps.Map(map_div, mapOptions);			
 
 	if(navigator.geolocation) {
 		//navigator.geolocation.getCurrentPosition(function(position){
 		//lat = position.coords.latitude;
 		//lon = position.coords.longitude;
-		//displayMap(map);
 		
 			map.panTo(myLocation);
 		
@@ -33,6 +31,7 @@ function init(){
 				infoWindow.setContent(marker.title);
 				infoWindow.open(map, marker);
 			});
+			draw_stations(map);
 		//});
 	}
 	else {
@@ -40,20 +39,41 @@ function init(){
 		}
 }
 
+function draw_stations(map){
 
-function displayMap(map){
-	
-	map.panTo(myLocation);
-
-	var marker = new google.maps.Marker({
-		position: myLocation,
-		title: "I am here!"
-	});
-	
-	marker.setMap(map);
-	
-	google.maps.event.addListener(marker, 'click', function() {
-		infoWindow.setContent(marker.title);
-		infoWindow.open(map, marker);
-	});
+	xhr = new XMLHttpRequest();
+	xhr.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+	xhr.onreadystatechange = dataReady;
+	xhr.send(null);
 }
+
+function dataReady(){
+	if (xhr.readyState == 4 && xhr.status == 200) {
+			scheduleData = JSON.parse(xhr.responseText);
+			scheduleDom = document.getElementById("schedule");
+			
+			var myLocation = new google.maps.LatLng(lat, lon);	
+		}
+	else if (xhr.readyState == 4 && xhr.status == 500) {
+		scheduleDom = document.getElementById("schedule");
+		scheduleDom.innerHTML = '<p>Loading the data has failed. Please try again</p>';
+	
+				}
+	}
+}
+
+function parse_json(){
+	xhr = new XMLHttpRequest();
+	xhr.open("get", "/stations.json", true);
+	xhr.onreadystatechange = parse_stations;
+	xhr.send(null);
+}
+
+function parse_stations(){
+	if(xhr.readyState==4 && xhr.status == 200) {
+		console.log("json file properly opened");
+		data = JSON.parse(xhr.responseText);
+		scheduleDom = document.getElementById("schedule");
+		scheduleDom.innerHTML = data[1]["line"];
+	}
+}	
