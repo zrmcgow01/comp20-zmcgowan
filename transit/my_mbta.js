a8aef46c75c4c1847d5539faf03f4642713f3bde
zrmcgow01 <zachary.mcgowan@tufts.edu>
2014-03-11 17:05:37 -0400

    updated json link

--- transit/my_mbta.js
+++ transit/my_mbta.js
@@ -65,7 +65,7 @@ function parse_json(){
 	console.log("hi");
 	var sample = 6;
 	xhr = new XMLHttpRequest();
-	xhr.open("get", 'stations.json', false);
+	xhr.open("get", 'https://github.com/tuftsdev/comp20-zmcgowan/blob/master/transit/stations.json', true);
 	xhr.onreadystatechange = parse_stations;
 	xhr.send(null);
 }
